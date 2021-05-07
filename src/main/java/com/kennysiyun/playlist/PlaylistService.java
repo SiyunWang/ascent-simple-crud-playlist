cd4c4e3d30b99ab3893078f5c5d00a9f921e8dcb
Siyun Wang <wangsiyun0518@gmail.com>
2021-05-07 11:50:02 -0700

    implement the getSongById service

--- src/main/java/com/kennysiyun/playlist/PlaylistService.java
+++ src/main/java/com/kennysiyun/playlist/PlaylistService.java
@@ -2,9 +2,24 @@ package com.kennysiyun.playlist;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class PlaylistService {
+    List<Song> myPlaylist = new ArrayList<>();
+
+    public void addSong(Song song) {
+        myPlaylist.add(song);
+    }
+
     public Song getSongById(int anyInt) {
+        for (Song song : myPlaylist) {
+            if (anyInt == song.getId()) {
+                return song;
+            }
+        }
         return null;
     }
+
 }
